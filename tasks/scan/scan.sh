07ea048272657c87afbc899cba063cf775a809b0
Hemanth AVS <hemanth.avs@gmail.com>
2020-03-31 15:20:37 -0500

    moved ci files to its own repo

---
+++ tasks/scan/scan.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -eu
+
+sleep 60
+
+TAG_NAME=`cat helmchart-semver/version`
+
+echo https://${HARBOR_HOST}/api/repositories/hemanth/pks-demo/tags/${TAG_NAME}
+
+wget -O /tmp/scan.json --no-check-certificate  --header "Authorization: Basic ${HARBOR_PASSWORD}" --header "Content-Type: application/json" https://${HARBOR_HOST}/api/repositories/hemanth/pks-demo/tags/${TAG_NAME}
+
+Result=`cat /tmp/scan.json | yq r - "scan_overview.*.severity"`
+
+if [ "$Result" != "High" ] ; then
+    echo "Container Images don't have vulnerability of defined High threshold"
+else
+    echo "Container Images has vulnerability of severity of High and above. Failing the step"
+    exit 1
+fi
+
+if [ "$Result" != "Critical" ] ; then
+    echo "Container Images don't have vulnerability of Critical threshold. Passing the step."
+else
+    echo "Container Images has vulnerability of severity of Critical and above. Failing the step"
+    exit 1
+fi
