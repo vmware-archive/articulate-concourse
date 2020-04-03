481deb020cb6aeed383545180a52afe372cc69a9
Hemanth AVS <hemanth.avs@gmail.com>
2020-04-03 18:27:54 -0500

    Updated Deploy App

--- tasks/deploy-app/task.sh
+++ tasks/deploy-app/task.sh
@@ -6,10 +6,7 @@ echo "Login to PKS API [$PKS_API]"
 pks login -a "$PKS_API" -u "$PKS_CLI_USERNAME" -p "$PKS_CLI_PASSWORD" --skip-ssl-verification
 echo "Retrieve cluster credentials and configuration for [${PKS_CLUSTER_NAME}]"
 pks get-credentials "${PKS_CLUSTER_NAME}"
-if [[ "$PKS_SKIP_TLS_VERIFY" == "true" ]]; then
-kubectl config set-cluster "${PKS_CLUSTER_NAME}" --certificate-authority=""
-kubectl config set-cluster "${PKS_CLUSTER_NAME}" --insecure-skip-tls-verify=true
-fi
+
 echo "Switch kubectl context to [${PKS_CLUSTER_NAME}]"
 kubectl config use-context ${PKS_CLUSTER_NAME}
 kubectl cluster-info
@@ -20,7 +17,7 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scr
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-version=`cat ./code/articulate/version`
+version=`cat ./code/articulate/articulate-version`
 cd ./code
 echo "Deploying App - $version"
-helm upgrade --install ins1 articulate --set appVersion=$version
+helm upgrade --install articulate articulate --set appVersion=$version
