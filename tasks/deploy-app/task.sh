65c4eb569595463fede0fa02d0af01d978d6a17a
Hemanth AVS <hemanth.avs@gmail.com>
2020-04-03 19:49:03 -0500

    refactored tasks

--- tasks/deploy-app/task.sh
+++ tasks/deploy-app/task.sh
@@ -4,11 +4,11 @@ set -eu
 
 echo "Login to PKS API [$PKS_API]"
 pks login -a "$PKS_API" -u "$PKS_CLI_USERNAME" -p "$PKS_CLI_PASSWORD" --skip-ssl-verification
-echo "Retrieve cluster credentials and configuration for [${PKS_CLUSTER_NAME}]"
-pks get-credentials "${PKS_CLUSTER_NAME}"
+echo "Retrieve cluster credentials and configuration for [${k8s-cluster-name}]"
+pks get-credentials "${k8s-cluster-name}"
 
-echo "Switch kubectl context to [${PKS_CLUSTER_NAME}]"
-kubectl config use-context ${PKS_CLUSTER_NAME}
+echo "Switch kubectl context to [${k8s-cluster-name}]"
+kubectl config use-context ${k8s-cluster-name}
 kubectl cluster-info
 
 echo "Deploying Application..."
--- tasks/test-app/task.sh
+++ tasks/test-app/task.sh
@@ -4,14 +4,14 @@ set -eu
 
 echo "Login to PKS API [$PKS_API]"
 pks login -a "$PKS_API" -u "$PKS_CLI_USERNAME" -p "$PKS_CLI_PASSWORD" --skip-ssl-verification
-echo "Retrieve cluster credentials and configuration for [${PKS_CLUSTER_NAME}]"
-pks get-credentials "${PKS_CLUSTER_NAME}"
+echo "Retrieve cluster credentials and configuration for [${k8s-cluster-name}]"
+pks get-credentials "${k8s-cluster-name}"
 if [[ "$PKS_SKIP_TLS_VERIFY" == "true" ]]; then
-kubectl config set-cluster "${PKS_CLUSTER_NAME}" --certificate-authority=""
-kubectl config set-cluster "${PKS_CLUSTER_NAME}" --insecure-skip-tls-verify=true
+kubectl config set-cluster "${k8s-cluster-name}" --certificate-authority=""
+kubectl config set-cluster "${k8s-cluster-name}" --insecure-skip-tls-verify=true
 fi
-echo "Switch kubectl context to [${PKS_CLUSTER_NAME}]"
-kubectl config use-context ${PKS_CLUSTER_NAME}
+echo "Switch kubectl context to [${k8s-cluster-name}]"
+kubectl config use-context ${k8s-cluster-name}
 kubectl cluster-info
 
 echo "Test Application..."
