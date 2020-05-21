68bb1558d61c5338415068d243aca68f17ada2e4
Hemanth AVS <hemanth.avs@gmail.com>
2020-05-20 22:10:37 -0500

    updated test task

--- tasks/test-app/task.sh
+++ tasks/test-app/task.sh
@@ -17,4 +17,4 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scr
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-helm test ${helm_release}
+helm test ${helm_release} --namespace ${k8s_cluster_ns}
