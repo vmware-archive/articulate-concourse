cfdc97505018a62af38899d98432285eb706edf6
Hemanth AVS <hemanth.avs@gmail.com>
2020-10-19 20:18:53 -0500

    updated repo

--- tasks/switch-blue-green-traffic/task.sh
+++ tasks/switch-blue-green-traffic/task.sh
@@ -5,7 +5,7 @@ set -eux
 echo "Login to PKS API [$PKS_API]"
 pks login -a "$PKS_API" -u "$PKS_CLI_USERNAME" -p "$PKS_CLI_PASSWORD" --skip-ssl-verification
 echo "Retrieve cluster credentials and configuration for [${k8s_cluster_name}]"
-pks get-kubeconfig "${k8s_cluster_name}" -a api.pks.caas.pez.pivotal.io -u "$PKS_CLI_USERNAME" -p "$PKS_CLI_PASSWORD" -k 2>&1
+pks get-kubeconfig "${k8s_cluster_name}" -a api.tkgi.caas.pez.pivotal.io -u "$PKS_CLI_USERNAME" -p "$PKS_CLI_PASSWORD" -k 2>&1
 
 echo "Switch kubectl context to [${k8s_cluster_name}]"
 kubectl config use-context ${k8s_cluster_name}
